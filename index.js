8d685afe27db2e288ede286e958d5dfc382133f5
JellyBeans511 <morita.tsuyoshi.s5@elms.hokudai.ac.jp>
2022-06-15 17:40:39 +0900

    up

--- index.js
+++ index.js
@@ -1,47 +1,8 @@
-
-window.addEventListener('DOMContentLoaded', init);
-
-const width = 960;
-const height = 540;
-
-function init() {
-
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#myCanvas')
-  });
-  renderer.setSize(width, height);
-
-
-
-  const scene = new THREE.Scene();
-
-
-  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-  camera.position.set(0, 0, +1000);
-
-
-  const geometry = new THREE.SphereGeometry(300, 30, 30);
-  const material = new THREE.MeshStandardMaterial({color: 0xFF0000});
-
-  const mesh = new THREE.Mesh(geometry, material);
-
-  scene.add(mesh);
-
-
-  const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
-  directionalLight.position.set(1, 1, 1);
-
-  scene.add(directionalLight);
-
-  tick();
-
-
-  function tick() {
-
-    mesh.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
-
-    requestAnimationFrame(tick);
-  }
-}
+const conegeometry = new THREE.ConeGeometry( 60, 80, 100 );
+　const conematerial = new THREE.MeshPhongMaterial( {
+　　　　　　　　　　color: 0xFF0000,
+                    opacity: 0.8,
+                    transparent: true,
+                    side: THREE.DoubleSide,} );
+　const cone = new THREE.Mesh( conegeometry, conematerial );
+　scene.add( cone );
